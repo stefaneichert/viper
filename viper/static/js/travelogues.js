19ae29f16857ed3fd37c0b9485416ac1e243554a
Christoph Steindl <christoph.steindl@onb.ac.at>
2023-05-25 17:59:56 +0200

    Add travelogue example

---
+++ viper/static/js/travelogues.js
@@ -0,0 +1,16 @@
+travelogues = [
+  {
+    "Place": "Appin",
+    "ID": 196764,
+    "title": "",
+    "description": "Beispiel Travelogues",
+    "type": "",
+    "source": "",
+    "timestamp": 17640414,
+    "page": 2,
+    "link": "http://data.onb.ac.at/rec/AC10099625",
+    "id": 196764,
+    "metadata": "https://iiif.onb.ac.at/presentation/ABO/+Z196807705/manifest/",
+    "image": ""
+  }
+]
