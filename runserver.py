135e42aac045280726ffe48d5025c74630548b9a
bkoschicek <b.koschicek@gmail.com>
2023-05-25 16:40:24 +0200

    removed freeze

--- runserver.py
+++ runserver.py
@@ -1,12 +1,4 @@
-import sys
-
 from viper import app
-from flask_frozen import Freezer
-
-freezer = Freezer(app)
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1 and sys.argv[1] == "build":
-        freezer.freeze()
-    else:
-        app.run()
+    app.run()
